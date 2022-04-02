a72308db828c2e4ffdede1c4d86f7777cd32876e
Szymon <szymonsawczuk@MacBook-Air-Szymon.local>
2022-04-02 16:54:06 +0200

    Add result dor lab2

---
+++ head/src/main.rs
@@ -0,0 +1,51 @@
+use clap::Parser;
+use std::io;
+use std::io::prelude::*;
+
+/// Head program
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// How many lines to write out
+    #[clap(long, default_value_t = 5)]
+    lines: usize,
+
+    /// Don't show error
+    #[clap(short)]
+    e: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+    let lines_of_text: Vec<String> = io::stdin()
+        .lock()
+        .lines()
+        .map(|x| match x {
+            Result::Ok(x) => x,
+            Result::Err(err) => {
+                eprintln!("Can't read line: {}", err);
+                std::process::exit(2);
+            }
+        })
+        .collect();
+
+    let index_end = if args.lines < lines_of_text.len() {
+        args.lines
+    } else {
+        lines_of_text.len()
+    };
+
+    lines_of_text[0..index_end]
+        .iter()
+        .for_each(|elem| println!("{}", elem));
+
+    if args.lines > lines_of_text.len() {
+        if !args.e {
+            eprintln!(
+                "Zabraklo {} linii do wypisania",
+                args.lines - lines_of_text.len()
+            );
+        }
+        std::process::exit(2);
+    }
+}
